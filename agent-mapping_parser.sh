d0598882e6185984f22c376e7cb5e4afdf5dc9e2
Raul Dimar <dimarr@anz.com>
2016-04-18 16:59:04 +1000

    fix the grep text

--- agent-mapping_parser.sh
+++ agent-mapping_parser.sh
@@ -2,7 +2,7 @@
 # Validate syntax of xml files...
 START=${1:-"modules/*/files"}
 find $START -name "domains.xml" |  xargs grep "agent mapping" | awk -F\" '{print $(NF-1)}' | python /app/ecetera/builder/bamboo-agent-home/build-dir/scripts/isRegex.py 1> .tmp/agent-mapping_parser.log 2>&1
-value=$( grep -c "failed \?: " .tmp/agent-mapping_parser.log )
+value=$( grep -c "failed" .tmp/agent-mapping_parser.log )
 echo "$value Bad regex found in XML files"
 echo $value > .tmp/agent-mapping_parser.err
 
